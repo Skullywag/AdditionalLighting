ca405b81f847e212e1fdf14c248c9550cdb6b07e
Skullywag <mjskull@gmail.com>
2016-01-01 09:11:34 +0000

    offset walllight so it can sit on walls, made path lamp depend on an internal charge from solar is now effected by eclipses etc

---
+++ Source/AdditionalLighting/Building_PathLamp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+using Verse;
+using RimWorld;
+namespace AdditionalLighting
+{
+    public class Building_PathLamp : Building
+    {
+        public decimal energy = 0m;
+
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+        }
+
+        public override void TickRare()
+        {
+            base.TickRare();
+            if(SkyManager.CurSkyGlow >= 0.6f)
+            {
+                energy += 250m;
+                if(energy >= 14000m)
+                {
+                    energy = 14000m;
+                }
+                this.GetComp<CompGlower>().ReceiveCompSignal("PowerTurnedOff");
+            }
+            else
+            {
+                energy -= 250m;
+                if(energy <= 0m)
+                {
+                    energy = 0m;
+                    this.GetComp<CompGlower>().ReceiveCompSignal("PowerTurnedOff");
+                    return;
+                }
+                this.GetComp<CompGlower>().ReceiveCompSignal("PowerTurnedOn");
+            }
+        }
+
+        public override string GetInspectString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            string inspectString = base.GetInspectString();
+            if (!inspectString.NullOrEmpty())
+            {
+                stringBuilder.AppendLine(inspectString);
+            }
+            decimal energyPercent = (energy / 14000m) * 100m;            
+            stringBuilder.AppendLine("Power = " + energyPercent.ToString("F") + "%");
+            return stringBuilder.ToString();
+        }
+    }
+}
+
